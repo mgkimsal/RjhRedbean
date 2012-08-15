8eb371a2ceb16bd5a352ef51f9d4f7f7ec89b275
Richard Holloway <richard@richardjh.org>
2012-08-15 21:40:07 +0100

    Use composer to install RedbeanPHP

---
+++ Module.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace RjhRedbean;
+
+class Module
+{
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
+    public function getAutoloaderConfig()
+    {
+        include __DIR__ . '/vendor/autoload.php';
+
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+}
