5c1b2000835a82ef67d37946a3e9462c15c535a1
Richard Holloway <richard@richardjh.org>
2013-05-22 22:19:43 +0100

    Added initializers and removed old code

--- config/module.config.php
+++ config/module.config.php
@@ -5,5 +5,8 @@ return array(
         'factories' => array(
             'RjhRedbean' => 'Richardjh\RjhRedbean\Service\RjhRedbeanServiceFactory',
         ),
+        'initializers' => array(
+            'Richardjh\RjhRedbean\Initializer\RedbeanAwareInitializer' => 'Richardjh\RjhRedbean\Initializer\RedbeanAwareInitializer',
+        ),
     ),
 );
